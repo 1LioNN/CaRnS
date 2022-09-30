ba4edca6e723ac7d771dc4f969b08af80486f90f
youssefisk <youssefmagdy24@yahoo.com>
2022-09-30 15:29:25 -0400

    CAR-1, 15 - email/password validation

--- CaRnS/server/models/authenticationModel.js
+++ CaRnS/server/models/authenticationModel.js
@@ -1,4 +1,5 @@
  const mongoose = require('mongoose')
+ const validator = require('validator')
 
  const Schema = mongoose.Schema
 
@@ -16,15 +17,27 @@
 
 // static signup method
 authenticationSchema.statics.signup = async function(email, password) {
+    // validation
     if (!email || !password) {
         throw Error('All fields must be filled')
     }
 
+    if(!validator.isEmail(email)){
+        throw Error('Email is not valid')
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password is not strong enough')
+    }
+
+
+    // checking if user exists
     const exists = await this.findOne({ email })
     if (exists){
         throw Error('Email already in use')
     }
 
+    // create user
     const user = await this.create({ email, password})
     return user
 }
