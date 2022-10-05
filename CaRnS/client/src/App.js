78ef7eda3000067812e48c3e9df9caa7e364ea3c
1LioNN <88006321+1LioNN@users.noreply.github.com>
2022-10-04 23:23:59 -0400

    Revert "CAR-40 Landing Page and Frontend Foundation"

--- CaRnS/client/src/App.js
+++ CaRnS/client/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/pages/LandingPage';
-import BuyListing from './components/pages/BuyListing';
-import RentListing from './components/pages/RentListing';
-import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<LandingPage/>}/>
-        <Route path='/buy' element={<BuyListing/>}/>
-        <Route path='/rent' element={<RentListing/>}/>
-        <Route path='/signup' element ={<SignUp/>}/>
-      </Routes>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
 export default App;
