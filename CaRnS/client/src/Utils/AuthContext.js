7b4dd1f35001e3e0cb05f8c4d65105a852f925c7
Chenbo Wang <chenbowang@Chenbos-MacBook-Pro.local>
2022-10-04 23:32:30 -0400

    CAR-1-2-15-16 fixed frontend api url

--- CaRnS/client/src/Utils/AuthContext.js
+++ CaRnS/client/src/Utils/AuthContext.js
@@ -10,7 +10,7 @@ export function AuthProvider( {children} ) {
     let [user, setUser] = useState(null);
 
     const signup = async ( { email, password, userType }, callback ) => {
-      const response = await fetch('localhost:5000/api/signup', {
+      const response = await fetch('localhost:5000/api/user/signup', {
         method: 'POST', 
         mode: 'cors',
         headers: {
@@ -28,7 +28,7 @@ export function AuthProvider( {children} ) {
     }
 
     let login = async ( { email, password } , callback) => {
-      const response = await fetch('localhost:5000/api/login', {
+      const response = await fetch('localhost:5000/api/user/login', {
         method: 'POST', 
         mode: 'cors',
         credentials: 'include',
