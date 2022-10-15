cda702445c95b0c3254e8a5818c736d7af17264b
Chenbo Wang <chenbowang@Chenbos-MacBook-Pro.local>
2022-10-15 16:01:00 -0400

    CAR-7-server side of pulling detail listing info done

--- CaRnS/server/routes/listingRoutes.js
+++ CaRnS/server/routes/listingRoutes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 //controller functions
-const { postBuyListing, postRentListing, viewBuyListings, viewRentListings, updateBuyListing, updateRentListing, deleteListing } = require('../controllers/listingController')
+const { postBuyListing, postRentListing, viewBuyListings, viewRentListings, updateBuyListing, updateRentListing, deleteListing, getdetailbuy } = require('../controllers/listingController')
 
 router.post('/post-buy', postBuyListing)
 router.post('/post-rent', postRentListing)
@@ -11,6 +11,7 @@ router.post('/post-rent', postRentListing)
 
 router.get('/view-buy', viewBuyListings)
 router.get('/view-rent', viewRentListings)
+router.get('/view-detail-buy/:id', getdetailbuy)
 
 router.put('/update-buy', updateBuyListing)
 router.put('/update-rent', updateRentListing)
