8047dc3e611389da2b7cf13342a4af5781d5a0ed
1LioNN <lionsu1995@gmail.com>
2022-10-18 13:35:16 -0400

    CAR-17 Fixed CSS formatting

--- CaRnS/client/src/components/Profile.js
+++ CaRnS/client/src/components/Profile.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import {useAuth} from '../Utils/AuthContext.js'
+import React from "react";
+import { useAuth } from "../Utils/AuthContext.js";
+import "./Profile.css"
+import defaultavatar from "../assets/icons/defaultavatar.png";
+import { Link } from "react-router-dom"
 
-function Profile(){
-    
-    const auth = useAuth();
-    return(
-        <div>
-            {auth.user ?
-            <>
-            <h1 style={{color: 'black'}}> Profile </h1>
-            <h1 style={{color: 'black'}}> E-mail: {auth.user.email} </h1>
-            <h1 style={{color: 'black'}}> Account Type: {auth.user.userType}</h1>
-            <h1 style={{color: 'black'}}> Account Creation Date: {auth.user.createdAt} </h1>
-                </>: <><h1 style={{color: 'black'}}> Please Log In </h1></>}
-        </div>
-    );
+function Profile() {
+  const auth = useAuth();
+  return (
+    <profile className="profile">
+      <div className="profile-container">
+        <a className="page-title">Profile</a>
+        <img className="profile-avatar"
+                  style={{ width: 175, height: 175 }}
+                  src={defaultavatar}
+                  alt="default avatar"
+        ></img>
+        <h1 style={{color: 'black'}}> E-mail: {auth.user.profile.name} </h1>
+      </div>
+    </profile>
+  );
 }
 
 export default Profile;
