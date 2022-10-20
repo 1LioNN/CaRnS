cfe00f815582d0a425bb76397bffe9b6a0292011
Chenbo Wang <chenbowang@Chenbos-MacBook-Pro.local>
2022-10-20 02:52:47 -0400

    CAR-1-2-15-16-keep logged in after refresh, fixed the bug where cookie is not stored in browser, implement log out

--- CaRnS/client/src/components/Profile.js
+++ CaRnS/client/src/components/Profile.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import {useAuth} from '../Utils/AuthContext.js'
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useNavigate } from 'react-router-dom';
 
 function Profile(){
-    
-    const auth = useAuth();
+    let auth = useAuth();
+    let navigate = useNavigate();
+    const onClick = async () => {
+        auth.logout((res, data)=> {
+            if (res === 200) {
+                navigate('/');
+            }
+            else {
+                
+            }
+        });
+    };
     return(
         <div>
             {auth.user ?
@@ -15,6 +28,24 @@ function Profile(){
             <h1 style={{ color: 'black' }}> Phone Number: {auth.user.profile.phone_number} </h1>
             <h1 style={{color: 'black'}}> Account Type: {auth.user.userType}</h1>
             <h1 style={{color: 'black'}}> Account Creation Date: {auth.user.createdAt} </h1>
+            <Typography align='center'>
+                <Button
+                className="btn"
+                style={{
+                    borderRadius: 40,
+                    backgroundColor: "#e87123",
+                    padding: "8px 30px",
+                    fontSize: "18px",
+                    color: "#fff"
+                }}
+                variant="contained"
+                disableElevation
+                onClick={onClick}
+                >
+                Log Out
+                </Button>
+                </Typography>
+            
                 </>: <><h1 style={{color: 'black'}}> Please Log In </h1></>}
         </div>
     );
