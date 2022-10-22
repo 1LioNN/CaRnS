48270be28a0981ec06c0bc7413704bbcb7f549ae
Zexi Zhang <zhang.zexi@hotmail.com>
2022-10-21 23:55:05 -0400

    refined views of each listings

--- CaRnS/client/src/components/pages/BuyListing.js
+++ CaRnS/client/src/components/pages/BuyListing.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+// import for card component
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 import { useEffect, useState} from 'react'
 
 function BuyListings(){
@@ -27,16 +35,33 @@ function BuyListings(){
         fetchBuyListings()
     }, [])
 
+
     return(
         <buylisting>
             <h1 style={{color: 'black'}}> Buy </h1>
             <div className='listings'>
                 {buyListing && buyListing.map((buyListing) => (
-                    <p key={buyListing._id}>
-                        <Link to={'/buydetail/'+buyListing._id}>
-                            {buyListing.listingName}
-                        </Link>
-                    </p>
+
+                    // Naive implementation of listing
+                    // <p key={buyListing._id}>
+                    //     <Link to={'/buydetail/'+buyListing._id}>
+                    //         {buyListing.listingName}
+                    //     </Link>
+                    // </p>
+
+                    <Card sx={{ minWidth: 275 }}>
+                        <CardContent>
+                            <Typography variant="h5" component="div">
+                                {buyListing.listingName}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <a href={'/buydetail/'+buyListing._id} target="_blank">
+                                <Button> More Detail </Button>
+                            </a>
+                        </CardActions>
+                    </Card>
+
                 ))}
             </div>
         </buylisting>        
