7ca55696fd7796a6702de27b3d52a02a028edeef
1LioNN <lionsu1995@gmail.com>
2022-11-02 21:55:30 -0400

    CAR-22 Finished styling Listing pages

--- CaRnS/client/src/components/ActiveSellList.js
+++ CaRnS/client/src/components/ActiveSellList.js
@@ -1,55 +1,78 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import "./Profile.css";
+import "./ActiveSellList.css";
 import ActiveHistorySingle from "./ActiveHistorySingle";
-import Stack from '@mui/material/Stack';
-import { useEffect, useState} from 'react'
+import Stack from "@mui/material/Stack";
+import { useEffect, useState } from "react";
 import { useAuth } from "../Utils/AuthContext.js";
+import AddIcon from "@mui/icons-material/Add";
 
-function ActiveSellList(){
-    const [buyListing, setBuyListings] = useState(null)
-    const auth = useAuth();  
-    useEffect(() => {
-        const fetchBuyListings = async () => {
-            const response = await fetch('http://localhost:8000/api/listing/viewActiveBuyListings/'+auth.user._id, {
-                method: 'GET',
-                mode: 'cors',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-
-            const data = await response.json()
-
-            if (response.ok){
-                setBuyListings(data)
-
-            }
+function ActiveSellList() {
+  const [buyListing, setBuyListings] = useState(null);
+  const auth = useAuth();
+  useEffect(() => {
+    const fetchBuyListings = async () => {
+      const response = await fetch(
+        "http://localhost:8000/api/listing/viewActiveBuyListings/" +
+          auth.user._id,
+        {
+          method: "GET",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
+      );
 
-        fetchBuyListings()
-    })
-    if(! auth.user){
-        console.log(1)
-        return (<>Loading</>)
-    }
+      const data = await response.json();
 
-    return(
-        <activeselllist>
-        <div className="profile-container">
-        <Link to={'/vendorrenthistory'}>Rent</Link>
-        <text className="page-title">Active Sell Listings</text>
-            <div className='listings'>
-            <Stack spacing={2}>
-                {buyListing && buyListing.map((buyListing) => (
-                    <ActiveHistorySingle listing = {buyListing}/>
+      if (response.ok) {
+        setBuyListings(data);
+      }
+    };
 
-                ))}
-        </Stack>
-            </div>
-            </div>
-        </activeselllist>        
-    );
+    fetchBuyListings();
+  });
+  if (!auth.user) {
+    console.log(1);
+    return <>Loading</>;
+  }
+
+  return (
+    <activeselllist>
+      <div className="listing-container">
+        <div className="page-header">
+          <text className="listing-page-title">
+            Active Sell Listings
+          </text>
+
+          <Link to={"/listingrent"}>
+            <text className="rentToggle">
+            View Active Rent Listings
+            </text> 
+          </Link>
+        </div>
+
+        <Link to={"/createlisting"}>
+          <div className="newListing">
+            <AddIcon
+              sx={{ fontSize: 50, marginTop: "20px", marginLeft: "47.5%" }}
+            />
+            New Sell Listing
+          </div>
+        </Link>
+
+        <div className="listings">
+          <Stack spacing={3}>
+            {buyListing &&
+              buyListing.map((buyListing) => (
+                <ActiveHistorySingle listing={buyListing} />
+              ))}
+          </Stack>
+        </div>
+      </div>
+    </activeselllist>
+  );
 }
 
 export default ActiveSellList;
